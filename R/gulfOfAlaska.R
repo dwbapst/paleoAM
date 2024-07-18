49c44f2a0929a44750537a81e2133c5e773cc285
David Bapst <dwbapst@gmail.com>
2024-07-18 12:32:19 -0500

    Update gulfOfAlaska.R

--- R/gulfOfAlaska.R
+++ R/gulfOfAlaska.R
@@ -1,7 +1,48 @@
-#' Foram Abundances from the Gulf of Alaska Reported by Sharon et al. 2021
+#' Benthic Foram Abundances from the Gulf of Alaska Reported by Sharon et al. 2021
 #' 
-#' Loads the abundance data collected and published by Sharon et al. (2021), as used in the simulation analyses published in Belanger and Bapst (2023).
+#' Loads the foraminifera abundances collected and published by Sharon et al. (2021),
+#'  as used in the simulation analyses published in Belanger and Bapst (2023).
+
+#' @details 
+#' This data set contains the absolute abundances (number of specimens identified)
+#' for 48 species of benthic foraminifera, across 355 samples, taken from Sharon 
+#' et al (2021). These samples were collected from the less than 63 μm size fractions
+#' taken from Integrated Ocean Drilling Program Expedition 341 Site U141 and the 
+#' co-located jumbo piston core, respectively located at 697 meters and 682 meters
+#'  water depth in the Gulf of Alaska (Jaeger et al., 2014).
+
+#' @name gulfOfAlaska
+
+#' @rdname gulfOfAlaska
+
+#' @aliases gulfOfAlaska fullDataTable_GOA DCA1_GOA abundData_GOA
+
+#' @docType data
+
+#' @format This dataset is composed of three objects: 
 #' 
+#' \describe{
+#' \item{fullDataTable_GOA}{The full data table containing sample IDs, DCA-1
+#' scores and species abundances.}
+#' 
+#' \item{DCA1_GOA}{A vector of just the DCA-1 scores for each sample.}
+#' 
+#' \item{abundData_GOA}{The number of specimens identified as a particular
+#'  species for each sample.}}
+#' 
+
+#' @source 
+#' Belanger and Bapst.
+#' 
+#' Jaeger et al., 2014
+#' 
+#' Sharon et al. 2021
+
+# @seealso 
+
+
+#' @keywords datasets
+
 #' @examples
 #' data(gulfOfAlaska)
 #' 
@@ -22,3 +63,4 @@
 #' 
 #' }
 #' 
+NULL
