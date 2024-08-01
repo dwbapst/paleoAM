e7e628aa5e49f2a39d65c9792aca99a50aab9119
David Bapst <dwbapst@gmail.com>
2024-08-01 12:54:36 -0500

    new function for plotting KDEs

---
+++ R/plotGradientKDE.R
@@ -0,0 +1,45 @@
+# Plot Kernel Density Estimates of Species Abundance Across a Focal Gradient
+
+plotGradientKDE <- function(
+        speciesKDEs,
+        fullGradientRange,
+        xlim = NULL,
+        ylim = c(0, 1),
+        logy = FALSE
+        ){
+
+    if(is.null(xlim)){
+        xlim <- fullGradientRange
+        }
+    
+    if(logy){
+        plot(speciesKDEs[[1]],
+             #main = "Scaled KDEs of Abundance for Every Species in the Dataset",
+             main = "",
+             ylab = "Scaled Expected Abundance If Present",
+             xlab = "Gradient (DCA-1)",
+             xlim = xlim,
+             ylim = ylim,
+             lwd = 2
+             ,log = "y"
+             )
+    }else{
+        plot(speciesKDEs[[1]],
+             #main = "Scaled KDEs of Abundance for Every Species in the Dataset",
+             main = "",
+             ylab = "Scaled Expected Abundance If Present",
+             xlab = "Gradient (DCA-1)",
+             xlim = xlim,
+             ylim = ylim,
+             lwd = 2
+             #,log = "y"
+             )
+        }
+
+    for(i in 2:length(speciesKDEs)){
+        lines(speciesKDEs[[i]], 
+            col = i, lwd = 2
+            )
+        }
+
+    }
